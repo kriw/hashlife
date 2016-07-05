5e409b04f0179a67854391e129ed9944c993ec14
Shohei Kuroiwa <shoheikuroiwa@Shohei-no-MacBook-Air.local>
2016-07-05 20:21:45 +0900

    change branch

--- lifegame/src/simple/field.rs
+++ lifegame/src/simple/field.rs
@@ -6,6 +6,8 @@ pub fn run(){
     let mut cells: [i32; cellnum] = [1; cellnum];
     let mut newcells: [i32; cellnum] = [1; cellnum];
 
+    setCell(&cells,3,3);
+
     for i in 1..2 {
         if update(&cells,&newcells) {
         } else {
@@ -15,7 +17,11 @@ pub fn run(){
 
 }
 
-pub fn update(cells: &[i32; cellnum],newcells: &[i32; cellnum]) -> bool{
+pub fn setCell(cells: &[i32; cellnum],x: usize, y: usize){
+    cells[x+cellnum*y] = 1;
+}
+
+pub fn update(cells: &[i32; cellnum],newcells: &[i32; cellnum]) -> i32{
     let s: i32 = (size+1) as i32;
     let mut sum = 0;
 
@@ -25,20 +31,22 @@ pub fn update(cells: &[i32; cellnum],newcells: &[i32; cellnum]) -> bool{
             let uy = y as usize;
             sum = neighbors(cells,ux,uy);
 
-            let temp: i32 = if cells[ux+size*uy] == 1 {
-                                 if sum == 2 || sum == 3 { 1 } else {0}
-                             } else {
-                                 if sum == 3 { 1 } else {0}
-                             };
+            let temp: i32;
+
+            match (cells[ux+size*uy] , sum) {
+                (0,3) | (1,2) | (1,3) => temp = 1,
+                _ => temp = 0
+            }
         }
     }
-    return true;
+    return temp;
 }
 
 fn neighbors(cells: &[i32; cellnum] ,x: usize , y: usize) -> i32 {
 
     let population : i32;
     let mut count = -cells[ (x + size*y) as usize ];
+
     for dx in -1..2 {
         for dy in -1..2 {
             let nx : usize = (x as i32 + dx) as usize;
